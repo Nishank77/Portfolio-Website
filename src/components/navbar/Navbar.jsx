09faa577514bf2c3db8217363d88f913e4904e37
Nishank77 <nishank.kose77@gmail.com>
2025-02-21 18:39:04 +0530

    2nd Commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.css'
+import { getImageUrl } from "../../utils.js"
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+
+
   return (
-    <>
-        <nav className={styles.Navbar}>
-            <a href='/'>Portfolio</a> 
-            <div className={styles.menu}>
-                <ul className={styles.menuItems}>
-                    <li><a href='#about'>About</a></li>
-                    <li><a href='#experience'>Experience</a></li>
-                    <li><a href='#projects'>Projects</a></li>
-                    <li><a href='#contact'>Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-    </>
+
+    <nav className={styles.navbar}>
+      <a href='/' className={styles.title}>Portfolio</a>
+      <div className={styles.menu}>
+        {/* {<img className={styles.menuBtn} src={getImageUrl('nav/menuIcon.png')} alt="menu-button" />} */}
+        <img className={styles.menuBtn} src={menuOpen ? "/assets/nav/closeIcon.png" : "/assets/nav/menuIcon.png"} alt="menu-button" onClick={() => { setMenuOpen(!menuOpen) }} />
+
+        <ul className={styles.menuItems}>
+          <li><a href='#about'>About</a></li>
+          <li><a href='#experience'>Experience</a></li>
+          <li><a href='#projects'>Projects</a></li>
+          <li><a href='#contact'>Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
   )
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 //Method used to make Absolute Path from the Relative Path
 
 export const getImageUrl = (path) => {
-    return new URL(`assets/${path}`, import.meta.url).href;
+    return new URL(`/assets/${path}`, import.meta.url).href;
 }
