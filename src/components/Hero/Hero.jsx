25b0957884092666faf3dec7a616768b08130929
nishank77 <nishank.kose77@gmail.com>
2025-03-06 00:09:43 +0530

    i think this should be final

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,12 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I&apos;m Nishank</h1>
+        <h1 className={styles.title} id="dynamic-title">
+          Hi, I&apos;m Nishank
+        </h1>
         <p className={styles.description}>
-          I&apos;m a Full Stack Developer with 5 experience with React, Java and
-          SpringBoot. Reach Out if you&apos;d like to learn more
+          I&apos;m a Full Stack Developer with an year of experience with React,
+          Java and SpringBoot. Reach Out if you&apos;d like to learn more
         </p>
         <a className={styles.contactBtn} href="mailto:nishank.kose77@gmail.com">
           Contact Me
